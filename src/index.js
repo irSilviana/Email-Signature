7369417f9ad9431a5aa06db582264a612c9a044d
irSilviana <76592439+irSilviana@users.noreply.github.com>
2021-02-14 20:52:53 +0700

    Update index.html
    Change telephone input type

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function capitalizeFirstLetter(words) {
-  var separateWord = words.toLowerCase().split(" ");
-  for (var i = 0; i < separateWord.length; i++) {
+  let separateWord = words.toLowerCase().split(" ");
+  for (let i = 0; i < separateWord.length; i++) {
     separateWord[i] =
       separateWord[i].charAt(0).toUpperCase() + separateWord[i].substring(1);
   }
